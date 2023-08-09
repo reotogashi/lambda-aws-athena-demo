039bc04b74035d7f416d8839d3f133adf957b97f
reo.togashi <reo.togashi@gecogeco.com>
2023-08-09 22:09:09 +0800

    add/format setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,14 @@
 module.exports = {
-    semi: true,
-    trailingComma: "all",
-    singleQuote: true,
-    printWidth: 120,
-    tabWidth: 4
-  };
+  printWidth: 80,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+  proseWrap: 'preserve',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'off',
+  singleAttributePerLine: false,
+};
--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,12 @@
  */
 
 export default {
-    transform: {
-        '^.+\\.ts?$': 'esbuild-jest',
-    },
-    clearMocks: true,
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageProvider: 'v8',
-    testMatch: ['**/tests/unit/*.test.ts'],
+  transform: {
+    '^.+\\.ts?$': 'esbuild-jest',
+  },
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testMatch: ['**/tests/unit/*.test.ts'],
 };
